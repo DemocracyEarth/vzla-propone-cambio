c7258bfad29b98e58874eb106d12711b38e27ee7
medied <medina.eag@gmail.com>
2018-05-08 22:39:42 -0400

    Update english language

--- js/custom.js
+++ js/custom.js
@@ -13,11 +13,11 @@ var dictionary = {
         },
 
         'manipulated-tallies-title': {
-            'en': "Tallies from the most recent elections have been verifiably manipulated",
+            'en': "Venezuela has collapsed",
             'es': "El pais está en colapso",
         },
         'manipulated-tallies-text': {
-            'en': "Smartmatic, the company that built the technology behind every Venezuelan election from 2004 to 2017, declared that the results of the 2017 Constituent Assembly election were manipulated “without any doubt”.<br/><br/>The government recently switched to a new system run by an unknown company, and conclusive proof of fraud was found.",
+            'en': "Venezuela collapsed and participating in an election under the framework of the regime only gives legitimacy to Maduro.<br/><br/>Smartmatic even declared that the results of the illegitimate Constituent Assembly election were manipulated “without any doubt”.",
             'es': "Venezuela ha colapsado y participar en las elecciones bajo el marco del régimen solo le da legitimidad a Maduro.<br/><br/>Smartmatic inclusive declaró que los resultados de la elección de la ilegítima Asamblea Nacional Constituyente fueron trastocados “sin duda alguna”.",
         },
         'manipulated-tallies-sources': {
@@ -28,11 +28,11 @@ var dictionary = {
         },
 
         'democracy-title': {
-            'en': "As official institutions take a dictatorial turn, Venezuelans continue to bet on democracy",
+            'en': "Venezuelans are still fighting for democracy",
             'es': "Los venezolanos seguimos luchando por democracia",
         },
         'democracy-text': {
-            'en': "We are building social consensus tools that are more transparent, efficient and impossible to corrupt.<br/>It's time to start proposing change and working for it.",
+            'en': "We are building social consensus tools that are more transparent, efficient and impossible to corrupt.<br/><br/>We are building be new realms for the democratic manifestation of ideas.",
             'es': "Estamos construyendo herramientas de consenso social mas transparentes, eficicientes e imposibles de corrumpir.<br/><br/>Estamos creando alternativas de gobernanza democrática para Venezuela más allá de la coyuntura política-electoral. ",
         },
         'learn-more-sources': {
@@ -41,12 +41,12 @@ var dictionary = {
         },
 
         'sovereign-title': {
-            'en': "Join the discussions at Sovereign",
-            'es': "Es hora de empezar a proponer cambio y a trabajar por ello",
+            'en': "It's time to propose change and work for it",
+            'es': "Es hora de proponer cambio y trabajar por ello",
         },
         'sovereign-text': {
-            'en': "Register at <a class='styless-anchor' href='https://venezuela.democracy.earth'>venezuela.democracy.earth</a> and participate in discussions you care about as a Venezuelan.",
-            'es': "Haz oir tu voz: crea tu cuenta en <a class='styless-anchor' href='https://venezuela.democracy.earth'>venezuela.democracy.earth</a> y participa en la propuesta de ideas por un cambio para Venezuela",
+            'en': "Raise your voice: register at <a class='styless-anchor' href='https://venezuela.democracy.earth'>venezuela.democracy.earth</a> and participate in the proposition of ideas for a change in Venezuela.",
+            'es': "Haz oir tu voz: crea tu cuenta en <a class='styless-anchor' href='https://venezuela.democracy.earth'>venezuela.democracy.earth</a> y participa en la propuesta de ideas por un cambio para Venezuela.",
         },
         'how-to-sources': {
             'en': `<li><a target='_blank' href='#'>How do I participate?</a></li>`,
